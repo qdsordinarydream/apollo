4d814221bc0d0a5ad4197269c1eac6f4250f8f80
dongsheng.qi <dongsheng.qi@mintegral.com>
2024-03-11 11:49:30 +0800

    feat: 更新版本

--- temp/start.sh
+++ temp/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-java -Dfile.encoding=UTF-8 -jar ../apollo-configservice/target/apollo-configservice-0.0.1-MTG.jar &
-java -Dfile.encoding=UTF-8 -jar ../apollo-adminservice/target/apollo-adminservice-0.0.1-MTG.jar &
-java -Dfile.encoding=UTF-8 -jar ../apollo-portal/target/apollo-portal-0.0.1-MTG.jar &
+java -Dfile.encoding=UTF-8 -jar ../apollo-configservice/target/apollo-configservice-0.1.0-MTG.jar &
+java -Dfile.encoding=UTF-8 -jar ../apollo-adminservice/target/apollo-adminservice-0.1.0-MTG.jar &
+java -Dfile.encoding=UTF-8 -jar ../apollo-portal/target/apollo-portal-0.1.0-MTG.jar &
 
 wait  # 等待所有进程完成
